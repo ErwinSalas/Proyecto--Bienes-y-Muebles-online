e32601d0af2d6fcb37f30718024dbd83d35458e8
ErwinSalas <erwinsalas42@gmail.com>
2016-11-05 15:17:46 -0600

    Añadir archivos

---
+++ Proyecto ll/app/Property.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Comment;
+class Property extends Model
+{
+    //
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+}
