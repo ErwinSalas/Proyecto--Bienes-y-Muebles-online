6bb75d175b5c5ae8f1017cd4bacb19f792b6ddca
ErwinSalas <erwinsalas42@gmail.com>
2016-11-05 17:58:20 -0600

    nuevo_proyecto

---
+++ app/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use \app\Comments;
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'name', 'email','type','state', 'password',
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
